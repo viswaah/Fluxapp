91b7ecf9526fb3ffad335837306827dfe771f76b
grassper <rishi.bharath27@gmail.com>
2022-11-16 23:20:59 +0530

    feat: local storage and appconfig added

---
+++ src/localStorage/localStorage.tsx
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export class LocalStorage {
+  static async setData(key: string, value: any) {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static async getData(key: string) {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      return JSON.parse(value);
+    }
+    return null;
+  }
+}
