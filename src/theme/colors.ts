1fe4527a6bd8d2f6d058ff77ae547e9702edabfb
viswaah <vishwajenvish@gmail.com>
2022-12-09 10:01:49 +0530

    style: change colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -7,9 +7,9 @@ export const colors = {
         success: '#138000'
     },
     bg: {
-        primary: '#E5D9B6',
-        secondary: '#A4BE7B',
-        tertiary: '#5F8D4E',
+        primary: '#FFF8F3',
+        secondary: '#A3E4DB',
+        tertiary: '#1C6DD0',
         disabled: '#9C9C9C',
         error: '#D0421B',
         success: '#138000'
